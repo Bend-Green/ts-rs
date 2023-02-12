5bbcc02be2ca74680318f106b5282b7fbd3958de
Charles Moore <Moore.CG@outlook.com>
2023-02-11 17:52:34 -0800

    Render Option<type> as 'type | undefined' (rather than 'type | null')

--- ts-rs/src/lib.rs
+++ ts-rs/src/lib.rs
@@ -402,11 +402,11 @@ impl<T: TS> TS for Option<T> {
             "called Option::name_with_type_args with {} args",
             args.len()
         );
-        format!("{} | null", args[0])
+        format!("{} | undefined", args[0])
     }
 
     fn inline() -> String {
-        format!("{} | null", T::inline())
+        format!("{} | undefined", T::inline())
     }
 
     fn dependencies() -> Vec<Dependency> {
